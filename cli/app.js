38937a01b6b8515a3860ab6716fdcc105f5f52e6
Narayanan M Ramanathan <narayananramu6@gmail.com>
2018-07-06 16:14:18 +0530

    import new command and execute

--- cli/app.js
+++ cli/app.js
@@ -1,10 +1,12 @@
 const program = require('commander');
+const newCommand = require('./new');
 
 program
     .command('new <name>')
     .alias('n')
     .description(`\tCreate a new IO Express application. "xprs new my_app" creates a new application called my_app in "./my_app"`)
     .action(function(name) {
+        newCommand(name);
     });
 
 if (process.argv.length < 3) {
