35944d60e56446086d49e77069aa9bb98d3ce05b
Narayanan M Ramanathan <narayananramu6@gmail.com>
2018-07-06 16:24:03 +0530

    validator file added

---
+++ cli/create/validator.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const validation = {
+    isIOExpress: function() {
+        return new Promise(function(resolve, reject) {
+            const controllerPath = path.join(process.cwd(), '/src/controller');
+            const handlerPath = path.join(process.cwd(), '/src/handler');
+            const middlewarePath = path.join(process.cwd(), '/src/middleware');
+            const modelsPath = path.join(process.cwd(), '/src/models');
+            const routesPath = path.join(process.cwd(), '/src/routes');
+            return (
+                fs.existsSync(controllerPath) && fs.existsSync(handlerPath) &&
+                fs.existsSync(middlewarePath) && fs.existsSync(modelsPath) &&
+                fs.existsSync(routesPath)
+            ) ? resolve(true) : reject(false);
+        });
+    },
+    ioExpressDirectoryExists: function(type, directoryName) {
+        return new Promise(function(resolve, reject) {
+            const directoryPath = path.join(process.cwd(), '/src', type, directoryName);
+            return (fs.existsSync(directoryPath)) ? resolve(true) : reject(false);
+        });
+    },
+    ioExpressFileExists: function(type, directoryName, fileName) {
+        return new Promise(function(resolve, reject) {
+            const filePath = path.join(process.cwd(), '/src', type, directoryName, fileName);
+            return (fs.existsSync(filePath)) ? resolve(true) : reject(false);
+        });
+    }
+}
+
+module.exports = validation;
