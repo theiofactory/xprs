585489ecc9fc09f400140591f7a9a323918ad06e
Narayanan M Ramanathan <narayananramu6@gmail.com>
2018-07-07 19:23:58 +0530

    index new route in route/index.js

--- cli/create/newRoute.js
+++ cli/create/newRoute.js
@@ -11,6 +11,17 @@ async function newRoute(name) {
     dummyRoute = dummyRoute.replace(/controller/g, 'index');
     dummyRoute = dummyRoute.replace('./index', `./../controller/${name}/index.controller`);
     fs.writeFileSync(path.join(process.cwd(), 'src/routes', `${name}.js`), dummyRoute);
+    // read & edit main route
+    let routeFile = fs.readFileSync(path.join(process.cwd(), 'src/routes/index.js')).toString();
+    routeFile = routeFile.split('\n');
+    // import controller
+    const importBeforeIndex = routeFile.findIndex(item => item.includes('const router =')) - 1;
+    routeFile = [...routeFile.slice(0, importBeforeIndex), `import ${name} from './${name}';`, ...routeFile.slice(importBeforeIndex, routeFile.length)];
+    // edit routes
+    const insertBeforeIndex = routeFile.findIndex(item => item.includes('export default')) - 1;
+    routeFile = [...routeFile.slice(0, insertBeforeIndex), `router.use('/${name}', ${name});`, ...routeFile.slice(insertBeforeIndex, routeFile.length)];
+    // save route
+    fs.writeFileSync(path.join(process.cwd(), 'src/routes/index.js'), routeFile.join('\n'));
 }
 
 module.exports = newRoute;
