96e67f9b50d373f7c8b2fd07e24d6bee4dcd0d36
fizan2904 <fizan.nagarchi6@gmail.com>
2018-10-25 19:29:38 +0530

    new dummy controller file

--- cli/dummy/controller.js
+++ cli/dummy/controller.js
@@ -1,3 +1,12 @@
-export default (request, response) => {
-    response.send('👍');
-};
+/**
+ * @function
+ * @name CONTROLLER
+ * @description CONTROLLER description.
+ * @param {request} - Request object.
+ * @param {response} - response object.
+ *
+ * @returns {String} - CONTROLLER return details
+ */
+const CONTROLLER = (request, response) => response.send('👍');
+
+module.exports = CONTROLLER;
