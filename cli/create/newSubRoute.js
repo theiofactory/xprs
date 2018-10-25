207c50805341d08c3b9de53eab7555c47cb74f42
fizan2904 <fizan.nagarchi6@gmail.com>
2018-10-25 19:34:38 +0530

    included testcase files and replaced variables name in them

--- cli/create/newSubRoute.js
+++ cli/create/newSubRoute.js
@@ -4,14 +4,19 @@ const path = require('path');
 async function newSubRoute(directory, name, method) {
     const importName = `${method}${name.charAt(0).toUpperCase()}${name.slice(1, name.length)}`;
     // read & copy controller route
-    const dummyController = fs.readFileSync(path.join(__dirname, '../dummy/controller.js')).toString();
-    fs.writeFileSync(path.join(process.cwd(), 'src/controller', directory, `${importName}.controller.js`), dummyController);
+    let dummyController = fs.readFileSync(path.join(__dirname, '../dummy/controller.js')).toString();
+    dummyController = dummyController.replace(/CONTROLLER/g, name);
+    fs.writeFileSync(path.join(process.cwd(), 'src/controllers', directory, `${importName}.controller.js`), dummyController);
+    // read & copy dummy e2e test
+    let dummyRouteE2ETest = fs.readFileSync(path.join(__dirname, '../dummy/route.spec.js')).toString();
+    fs.mkdirSync(path.join(process.cwd(), 'e2e', name));
+    fs.writeFileSync(path.join(process.cwd(), 'e2e', name, `${importName}.spec.js`), dummyRouteE2ETest);
     // read & edit route
     let routeFile = fs.readFileSync(path.join(process.cwd(), 'src/routes', `${directory}.js`)).toString();
     routeFile = routeFile.split('\n');
     // import controller
     const importBeforeIndex = routeFile.findIndex(item => item.includes('const router =')) - 1;
-    routeFile = [...routeFile.slice(0, importBeforeIndex), `import ${importName} from './../controller/${directory}/${importName}.controller';`, ...routeFile.slice(importBeforeIndex, routeFile.length)];
+    routeFile = [...routeFile.slice(0, importBeforeIndex), `import ${importName} from './../controllers/${directory}/${importName}.controller';`, ...routeFile.slice(importBeforeIndex, routeFile.length)];
     // edit routes
     const insertBeforeIndex = routeFile.findIndex(item => item.includes('export default')) - 1;
     routeFile = [...routeFile.slice(0, insertBeforeIndex), `router.${method}('/${name}', ${importName});`, ...routeFile.slice(insertBeforeIndex, routeFile.length)];
