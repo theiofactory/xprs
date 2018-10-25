de2a811cb30b4cbcd979748c797dc1eb16f4d637
fizan2904 <fizan.nagarchi6@gmail.com>
2018-10-25 19:33:13 +0530

    included testcase files and replaced variable names in them

--- cli/create/Others.js
+++ cli/create/Others.js
@@ -7,15 +7,26 @@ async function createOthers(type, name) {
     try {
         await Validator.isIOExpress();
         try {
-            await Validator.ioExpressFileExists(type, '', `${name}.${type}.js`);
+            await Validator.ioExpressFileExists((type[type.length-1] !== 's') ? type.charAt(0).toUpperCase() + type.slice(1) + 's' : type.charAt(0).toUpperCase() + type.slice(1), '', `${name}.${type}.js`);
             console.log(`You are trying to create a ${type} that already exists.`);
             return -1;
         } catch (e) {
             let dummyFile = fs.readFileSync(path.join(__dirname, '../dummy', `${type}.js`)).toString();
-            if (type === 'models') {
-                dummyFile = dummyFile.replace('schemaName', name)
+            let dummyFileTest = fs.readFileSync(path.join(__dirname, '../dummy', `${type}.spec.js`)).toString();
+            if (type === 'middleware') {
+                dummyFile = dummyFile.replace(/MIDDLEWARE/g, name);
+                dummyFileTest = dummyFileTest.replace(/MIDDLEWARE/g, name);
+                dummyFileTest = dummyFileTest.replace(`./${ name }`, `./${ name }.middleware`);
             }
-            fs.writeFileSync(path.join(process.cwd(), 'src', type, `${name}.${type}.js`), dummyFile)
+            if (type === 'model') {
+                dummyFile = dummyFile.replace('schemaName', name);
+                dummyFileTest = dummyFileTest.replace(/MODEL/g, name);
+                dummyFileTest = dummyFileTest.replace(`./${name}`, `./${name}.model`);
+                dummyFileTest = dummyFileTest.replace(/TESTNAME/g, `${name.charAt(0).toUpperCase() + name.slice(1)}Model`);
+                dummyFileTest = dummyFileTest.replace(/NEWDATA/g, `new${name.charAt(0).toUpperCase() + name.slice(1)}Model`);
+            }
+            fs.writeFileSync(path.join(process.cwd(), 'src', type + 's', `${name}.${type}.js`), dummyFile)
+            fs.writeFileSync(path.join(process.cwd(), 'src', type + 's', `${name}.${type}.spec.js`), dummyFileTest)
             return -1;
         }
     } catch (e) {
