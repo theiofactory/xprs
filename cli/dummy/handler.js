92b23a209e7473a86eb7e9b31c7408a7ae61a641
fizan2904 <fizan.nagarchi6@gmail.com>
2018-11-07 18:42:30 +0530

    removed trailing space

--- cli/dummy/handler.js
+++ cli/dummy/handler.js
@@ -7,7 +7,7 @@
  * @param {Object} request - Request object description.
  * @param {Object} response - Response object description.
  * @param {next} next - next function.
- * 
+ *
  * @return {Function} next function.
 */
 module.exports = (error, request, response, next) => {
